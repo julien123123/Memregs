b23e4ce091e3f731f56077d612112ecc7a0d5067
Julien Croteau <julien123123@gmail.com>
2025-09-20 21:40:15 -0400

    Corrections to pack class. Now everything works and tested. most attributes have been changed to inreg which is a 16 bits bytearray register that's easyer to use, and save!

--- memregs.py
+++ memregs.py
@@ -1,4 +1,4 @@
-import struct, json, uctypes
+import struct, json, uctypes, binascii
 
 #peut-être: sous-classes pour arguments en ordre de déclaration
 
@@ -160,7 +160,9 @@ class Pack(Mem):
         if not sav:
             self.items = {ar[0]: Memitem(i, *ar) for i, ar in enumerate(args)}
             self._order_items()
-            CACHE.push(self.name, {k: {attr: val for attr, val in v.__dict__.items() if attr not in ('memref', 'buf')} for k, v in self.items.items()}, self._id)
+            CACHE.push(self.name, {
+                k: {attr: (binascii.hexlify(val) if attr == "inreg" else val) for attr, val in v.__dict__.items() if
+                    attr not in ("memref", "buf")} for k, v in self.items.items()}, self._id)
         else:
             for k, d in sav.items():
                 self.items[k] = Memitem.from_dict(d, self)
@@ -178,30 +180,30 @@ class Pack(Mem):
         """
         word_cursor, bit_cursor = 0, 0
         # Process binary items first
-        for itm in sorted((i for i in self.items.values() if i.bin), key=lambda obj: obj.indx):
+        for itm in sorted((i for i in self.items.values() if i.inreg[0] & (1 <<7)), key=lambda obj: obj.indx):
             if bit_cursor >= 8:
                 word_cursor += 1
                 bit_cursor %= 8
-            itm.byte_pos = word_cursor
-            itm.bit_pos = bit_cursor
+            itm.inreg[3] = word_cursor
+            itm.inreg[0] |= bit_cursor & ((1 << 6)-1)
             itm.mask = 1 << bit_cursor
-            bit_cursor += itm.length
-            itm.memref = memoryview(self.buf)[itm.byte_pos:itm.byte_pos + 1]
+            bit_cursor += itm.inreg[2]
+            itm.memref = memoryview(self.buf)[itm.inreg[3]:itm.inreg[3] + 1]
             itm.buf = itm.raw_val
         # If bits are not aligned, move to next byte
         if bit_cursor > 0:
             word_cursor += 1
             bit_cursor = 0
         # Process non-binary items
-        for itm in sorted((i for i in self.items.values() if not i.bin), key=lambda obj: obj.indx):
-            itm.byte_pos = word_cursor
-            word_cursor += itm.length
-            itm.memref = memoryview(self.buf)[itm.byte_pos:itm.byte_pos + itm.length]
+        for itm in sorted((i for i in self.items.values() if not i.inreg[0] & (1<<7)), key=lambda obj: obj.indx):
+            itm.inreg[3] = word_cursor
+            word_cursor += (2**(itm.inreg[0]>>5)&0b11)*itm.inreg[2]
+            itm.memref = memoryview(self.buf)[itm.inreg[3]:itm.inreg[3] + itm.inreg[2]*(1 <<((itm.inreg[0] >>5)&0b11))]
             itm.buf = itm.raw_val
 
     def post_all(self):
         for v in self.items.values():
-            if v.bin:
+            if v.inreg[0] & 1 << 7 :
                 base_word = v.memref[0]
                 new_w = base_word | v.mask if v.buf == 1 else base_word & ~v.mask
                 v.memref[:] = struct.pack('B', new_w)
@@ -214,72 +216,84 @@ class Pack(Mem):
 class Memitem:
     #TODO: this class doesn't neet to have a buffer, it can write directly to memref, or pack_into
     #post all can alost just be the method in Mem
+    # BITPOS  = 5 BITS : inreg[0], bit 0-4
+    # BIN = 1 BIT : inreg[0], bit 7
+    # PACKFMT =  8 BITS: inreg[1]
+    # Packmultiplier = 2 Bits : inreg[0], bit 5, 6
+    # LENGTH = 8 BITS : inreg[2]
+    # BYTEPOS = 8 BITS : inreg[3]
 
-    __slots__ = ('indx','name','length','bin','pack_format','bit_pos','byte_pos','memref','buf','mask')
+    __slots__ = ('indx','name','length','bin','pack_format','memref','buf','mask', 'inreg')
     @classmethod
     def from_dict(cls,d, reg):
-        obj = cls(d['indx'], d['name'], d['length'], d.get('bin', False), d.get('pack_format', False))
-        obj.bit_pos = d['bit_pos']
-        obj.byte_pos = d['byte_pos']
-        obj.memref = memoryview(reg.buf)[obj.byte_pos:obj.byte_pos + (1 if obj.bin else obj.length)]
+        obj = cls(d['indx'], d['name'], 0, inreg=d['inreg'])
+        obj.length = obj.inreg[2]
+        obj.memref = memoryview(reg.buf)[obj.inreg[3]:obj.inreg[3] + (1 if (obj.inreg[0] & (1<<7)) else obj.inreg[2]*2**((obj.inreg[0]>>5)&0b11))]
         obj.buf = obj.raw_val
         return obj
-        
-    def __init__(self,indx, name, lenght, bin = False, pack_format=False):
+
+    def __init__(self,indx, name, lenght, bin = False, pack_format=False, inreg= None):
         self.indx = indx
         self.name = name
-        if lenght <= 0:
-            raise ValueError('item length must be greater than 0')
-        self.length = lenght
-        self.bin = bin
-        self.pack_format = pack_format
-        ### Variables that will be set by Memreg._order_items()
-        self.bit_pos = 0
-        self.byte_pos = 0
+        if not inreg:
+            self.inreg = bytearray(4)
+            if bin:
+                self.inreg[0] |= 1 << 7
+            if pack_format:
+                self.inreg[1] = ord(pack_format)
+            else:
+                self.inreg[1] = ord('B')
+            try:
+                self.inreg[0] |= ((len(struct.pack(chr(self.inreg[1]), 0)) >> 1) & 3) << 5
+            except TypeError:
+                self.inreg[0] |= ((len(struct.pack(chr(self.inreg[1]), 'B')) >> 1) & 3) << 5
+
+            self.inreg[2] = lenght
+        else:
+            self.inreg = binascii.unhexlify(inreg)
         self.memref = None
         self.mask = 0
         self.buf = 0 # Set to raw_val in Memreg
 
     @property
     def value(self): # parsing values here makes it less expensive to call
-        return struct.unpack(self.pack_format, bytes(self.memref))[0] if self.pack_format else bytes(self.memref) if not self.bin else bytes(self.memref)[0] >> self.bit_pos & 1
+        return struct.unpack(chr(self.inreg[1]), bytes(self.memref))[0] if self.inreg != ord('B') else bytes(self.memref) if not self.bin else bytes(self.memref)[0] >> self.bit_pos & 1
 
     @property
     def raw_val(self):
-        return bytes(self.memref) if not self.bin else bytes(self.memref)[0] >> self.bit_pos & 1
+        return bytes(self.memref) if not self.inreg[0] & (1<<7) else (bytes(self.memref)[0] >> (self.inreg[0] & 0b11111)) & 1
 
     def __str__(self):
-        return f'{self.name}: \n\t index = {self.indx}\n\t val ={self.value}\n\t byte_pos = {self.byte_pos}\n\t bin ={self.bin}\n\t bit_pos = {self.bit_pos}'
+        return f'{self.name}: \n\t index = {self.indx}\n\t val ={self.value}\n\t byte_pos = {self.inreg[3]}\n\t bin = {bool(self.inreg[0] >> 7)}\n\t bit_pos = {self.inreg[0]&0b11111}'
 
     def reset(self):
-        self.buf = bytearray([0x00] * self.length) if not self.bin else 0
+        self.buf = bytearray([0x00] * 2**((self.inreg[0]>>5) & 0b11)*self.inreg[2]) if not self.inreg[0] & (1<<7) else 0
 
     def ch_val(self, new_val):
         self.reset()
-        if self.bin:
-            self.buf = new_val & 1
-        elif self.pack_format:
-            self.buf = struct.pack(self.pack_format, new_val)
-        else:
-            if isinstance(new_val, (bytes, bytearray)):
-                self.buf = new_val
-            if isinstance(new_val, (str, int)):
-                self.buf = str(new_val).encode()
+        if self.inreg[0] & (1<<7):
+            self.buf = new_val & ((1 << self.inreg[1])-1)
+        elif isinstance(new_val, (bytes, bytearray)):
+            self.buf[:] = new_val
+        elif isinstance(new_val, (str)):
+            self.buf[:] = str(new_val).encode()
+        elif self.inreg[1] != ord('B'):
+            self.buf[:] = struct.pack(chr(self.inreg[1])*self.inreg[2], new_val)
 
     def toggle(self):
-        if not self.bin:
+        if not (self.inreg[0] >> 7) & 1:
             raise AttributeError('item is not defined as binary, cannot toggle!')
-        if self.length >1:
+        if self.inreg[2] >1:
             raise ValueError("item's length is superior to 1, cannot toggle")
-        self.buf = (self.raw_val + 1) % 2
+        self.buf = self.raw_val ^ 1
 
 if __name__ == "__main__":
     import os
     
-    b = bytearray(os.urandom(16))
+    b = bytearray(os.urandom(32))
     nvm = memoryview(b)
     nvam = Pack('nvam', nvm, 0, ('REPL', 1, True), ('DATA', 1, True), ('MNT', 1, True), ('SAC', 3), ('PLUS', 4), span =8)
-    mimi = Pack('mimi', nvm, 8, ('passw', 1), ('flag', 1, True), ('Nan', 4))
+    mimi = Pack('mimi', nvm, 8, ('passw', 10), ('flag', 1, True), ('Nan', 4))
     nvam['SAC'] = 'IOU'
     nvam.post_all()
     try:
